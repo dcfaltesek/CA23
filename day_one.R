905168f016a83daa4399737d06a6687251692260
Dan <danielfaltesek@gmail.com>
2023-09-28 16:32:19 -0700

    Create day_one.R
    code for the first day with real coding

---
+++ day_one.R
@@ -0,0 +1,23 @@
+#welcome to NMC 245
+#this is what a script file looks like, you don't need to be scared of it. 
+
+#today we will look at a few key functions
+
+#but first dan will give us a tour of the interface. 
+one<-""
+two<-""
+three<-""
+four<-""
+
+#how do we get things into four
+#command + enter OR highligh the line and press run
+
+#what if we want to use code that we didn't write or code to do more cool stuff?
+#we access packages, lets do that now
+library(nycflights13)
+
+#accessing help, how?
+
+#how many functions are in here...
+
+
