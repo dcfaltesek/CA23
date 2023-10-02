1ce214050c48fdf9ff6f23861753092558747287
Dan <danielfaltesek@gmail.com>
2023-10-02 11:52:12 -0700

    Add files via upload

---
+++ day_two.R
@@ -0,0 +1,13 @@
+#so let's go over our key skills
+
+#copy and paste shortcuts
+Apple-C and Apple-V (control for windows users)
+
+#run a line
+2+2
+
+#let's run a library
+library(nycflights13)
+
+#and assign something
+flights<-flights
